dd76654f7f4d9a753d8c754c89676b535ca0cc68
Ondrej Dockal <odockal@redhat.com>
2024-03-25 17:57:37 +0100

    changes for mac-related pipelines, moving the mac binary
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- lib/darwin/builder.sh
+++ lib/darwin/builder.sh
@@ -149,14 +149,19 @@ yarn compile
 
 # If all went well, there should be a podman desktop executable "Podman Desktop.exe" in dist/win-unpacked/
 expectedFilePath="$workingDir/podman-desktop/dist/mac-$architecture/"
-oldFileName="Podman Desktop.app"
-newFileName="pd.app"
+appName="Podman Desktop.app"
+oldFileName="Podman Desktop"
+newFileName="podman-desktop"
+oldBinaryPath="$expectedFilePath/$appName/Contents/MacOS/"
 # Write down the location of podman desktop executable into a file
-if [ -d "$expectedFilePath/$oldFileName" ]; then
+if [ -d "$oldBinaryPath/$oldFileName" ]; then
     # Rename the file
-    mv "$expectedFilePath/$oldFileName" "$expectedFilePath/$newFileName"
-    echo "The file has been renamed to $newFileName."
-    absolutePath=$(realpath "$expectedFilePath/$newFileName")  # Get the absolute path
+    mv "$oldBinaryPath/$oldFileName" "$oldBinaryPath/$newFileName"
+    echo "The Binary file has been renamed to $newFileName."
+    echo "Move/cp the binary to the workingDir: $workingDir"
+    cp "$oldBinaryPath/$newFileName" "$workingDir"
+    # Get the absolute path of the binary
+    absolutePath=$(realpath "$workingDir/$newFileName")  
     echo "The file exists at $absolutePath."
     cd "$workingDir/$resultsFolder" || exit
     # results directory should already exist
